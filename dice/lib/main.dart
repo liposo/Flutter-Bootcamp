92bab24f3cf6ed0a25b4307d9647ab29bda47588
Felipe <felipezb@live.com>
2020-04-19 01:49:08 +0300

    Finished dice app

--- dice/lib/main.dart
+++ dice/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "dart:math";
 
 void main() {
   return runApp(
@@ -15,24 +16,94 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  // List of all dice images;
+  var diceList = [
+    "images/dice1.png",
+    "images/dice2.png",
+    "images/dice3.png",
+    "images/dice4.png",
+    "images/dice5.png",
+    "images/dice6.png"
+  ];
+
+  // Generates a new Random object
+  final _random = new Random();
+
+  String rollDice() {
+    return diceList[_random.nextInt(diceList.length)];
+  }
+
+  String leftDice = "images/dice1.png";
+  String rightDice = "images/dice1.png";
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(children: [
-        Expanded(
-          child: FlatButton(
-            onPressed: () { print("Left pressed!"); },
-            child: Image.asset('images/dice1.png'),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+          child: Center(
+            child: Text(
+              "Touch each dice to roll, use the buttom to roll both at once.",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+            ),
           ),
         ),
-        Expanded(
-          child: FlatButton(
-            onPressed: () { print("Right pressed"); },
-            child: Image.asset('images/dice2.png'),
+        Row(children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDice = rollDice();
+                });
+              },
+              child: Image.asset(leftDice),
+            ),
           ),
-        )
-      ]),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  rightDice = rollDice();
+                });
+              },
+              child: Image.asset(rightDice),
+            ),
+          ),
+        ]),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: RaisedButton(
+            onPressed: () {
+              setState(() {
+                leftDice = rollDice();
+                rightDice = rollDice();
+              });
+            },
+            color: Colors.white,
+            padding: EdgeInsets.all(28.0),
+            child: Center(
+              child: Text(
+                "Roll both dices",
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
