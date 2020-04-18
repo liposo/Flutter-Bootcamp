975a7d24a23d10f6a792ccbd9f3cbdc3bbb0f5cb
Felipe <felipezb@live.com>
2020-04-18 13:02:11 +0300

    wrap image in FlatIconButton and set test callback

--- dice/lib/main.dart
+++ dice/lib/main.dart
@@ -18,13 +18,21 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      Expanded(
-        child: Image.asset('images/dice1.png'),
-      ),
-      Expanded(
-        child: Image.asset('images/dice2.png'),
-      )
-    ]);
+    return Center(
+      child: Row(children: [
+        Expanded(
+          child: FlatButton(
+            onPressed: () { print("Left pressed!"); },
+            child: Image.asset('images/dice1.png'),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () { print("Right pressed"); },
+            child: Image.asset('images/dice2.png'),
+          ),
+        )
+      ]),
+    );
   }
 }
