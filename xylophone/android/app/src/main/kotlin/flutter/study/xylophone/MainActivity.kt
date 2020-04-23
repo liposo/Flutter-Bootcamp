796f3e5edd6d5d0a79bf6b954b4242f322b487a3
Felipe <felipezb@live.com>
2020-04-23 20:33:34 +0300

    Section 9: Xylophone

---
+++ xylophone/android/app/src/main/kotlin/flutter/study/xylophone/MainActivity.kt
@@ -0,0 +1,6 @@
+package flutter.study.xylophone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ xylophone/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
+  var colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.teal,
+    Colors.blue,
+    Colors.purple
+  ];
+
+  void playSound(int note) {
+    player.play("note$note.wav");
+  }
+
+  Expanded buildKey(int note) {
+    return Expanded(
+      child: MaterialButton(
+        color: colors[note - 1],
+        onPressed: () {
+          playSound(note);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(1),
+              buildKey(2),
+              buildKey(3),
+              buildKey(4),
+              buildKey(5),
+              buildKey(6),
+              buildKey(7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
